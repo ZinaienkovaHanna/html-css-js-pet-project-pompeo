85ec1c636aef566971fb16d437835df609a0599a
Zinaienkova Hanna <zinaienkova.anna@gmail.com>
2022-09-01 16:06:10 +0300

    delete cart

--- js/online-store.js
+++ js/online-store.js
@@ -1,42 +1,6 @@
-(function () {
-  const products = [
-    {
-      id: '1',
-      title: 'Decor Plate',
-      image: 'img/online-store/pottery-collection-1.png',
-      price: 65.0,
-    },
-    {
-      id: '2',
-      title: 'Mint Pottery',
-      image: 'img/online-store/pottery-collection-2.png',
-      price: 75.0,
-    },
-    {
-      id: '3',
-      title: 'Set Of Potterys',
-      image: 'img/online-store/pottery-collection-3.png',
-      price: 125.0,
-    },
-    {
-      id: '4',
-      title: 'Orange Ceramic',
-      image: 'img/online-store/pottery-collection-4.png',
-      price: 55.0,
-    },
-    {
-      id: '5',
-      title: 'Dark Bowl',
-      image: 'img/online-store/pottery-collection-5.png',
-      price: 115.0,
-    },
-    {
-      id: '6',
-      title: 'Square Pottery',
-      image: 'img/online-store/pottery-collection-6.png',
-      price: 75.0,
-    },
-  ];
+(async function () {
+  const response = await fetch('api/online-store-products.json');
+  const products = await response.json();
 
   function renderProducts(products) {
     const productContainer = document.querySelector('.online-store__cards');
